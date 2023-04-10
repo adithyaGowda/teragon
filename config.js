9837922fc5fed3762c3c40b9da25805fdd61f060
adithyaGowda <52035861+adithyaGowda@users.noreply.github.com>
2023-04-10 12:57:41 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,186 @@
+const config = {
+  navbar: [
+    { title: "Home", uri: "/" },
+    { title: "Courses", uri: "/courses" },
+    { title: "Why Choose Us?", uri: "/why-choose-us" },
+    { title: "Events", uri: "/events" },
+    { title: "Contact Us", uri: "/contact-us" },
+  ],
+  header: {
+    text: "Join us for quality education with affordable prices!",
+    button: "Know More",
+    title: ["Teragon", "Edtech"],
+    imageUri: { hero1: "./img/hero2.png", hero2: "./img/hero4.png" },
+  },
+  aboutUs: {
+    title: "About Us",
+    desc: [
+      "Teragon was founded on a simple truth: Brilliance is evenly distributed, but opportunity is not. There are talented students everywhere, but all too often their potential is limited and more tangibly, through challenges of exposure to industrial knowledge, Teragon helps to bridge that gap as well as to accelerate the careers of potential minds.",
+      "We have sharpened our focus on bringing the best talent to all aspects of the digital product suite, roles that add tremendous value to the evolving world. Teragon is committed to teaching the skills of the future by making high-quality education accessible and affordable to individuals, students, and professionals around the world.",
+    ],
+    aboutImage: "./img/hero1.png",
+  },
+  missonVision: {
+    title: "Mission & Vision",
+    desc: [
+      "Our motivation is to identify the dream of students on what they want to achieve professionally right from their school level and to prepare them on the same to achieve it successfully.",
+      "The main vision is not only to provide the quality education but also to make it affordable to each and every individual in the country,and this will be our contribution towards helping students to learn professional courses.",
+    ],
+    missionImage: "./img/hero3.png",
+  },
+  strive: {
+    title: "What we strive for?",
+    desc: "EdTech, a growing industry in recent years, is finally having its moment. The 2020 crisis is casting a looming shadow of uncertainty over the education industry. Schools and colleges worldwide have been scrambling to adapt to remote or hybrid learning and the students are not getting the right education , which has increased demand for online courses, hence we strive to provide the various skilled courses to enhance their career path.",
+    subTopic: [
+      {
+        logo: "hand-fist",
+        title: "Empowerment",
+        desc: "We are driven by our belief in the power of free education and skills training to change the people’s lives for the better and are passionate about providing an overall learning experience that meets their needs and helps them to achieve life goals.",
+      },
+      {
+        logo: "book-bookmark",
+        title: "Knowledge",
+        desc: `We are experts in the field of online education and are rigorous in delivering high quality learning the materials, services and experiences that deliver the learning outcomes we have promised.
+          We believe that the best education has the power to break through the boundaries and to transform lives.`,
+      },
+      {
+        logo: "people-group",
+        title: "Inclusivity",
+        desc: "We are inspired by the saying, that “everyone is entitled to a free education”. We are committed to equality and access to education irrespective of gender, geography, economic status or any other barriers to access.",
+      },
+      {
+        logo: "lightbulb",
+        title: "Innovation",
+        desc: "We will not be constrained by what already exists but will lead the way in introducing new ways to achieve our mission. Our DNA is entrepreneurial and we understand and embrace the pioneering spirit that motivates other entrepreneurs.",
+      },
+    ],
+  },
+  studentsPlacedAt: "Our Students Placed At",
+  courses: {
+    desc: "As the world of digital learning accelerates, we in Teragon believe it is critical to study and understand the impact of the digital learning experience on student outcomes. Our dedicated impact research team is working tirelessly to understand this impact, and how digital learning can be improved and our courses and learning methods are designed and explained in a manner where it enhances the students or individual career.",
+    courseOptions: [
+      {
+        title: "Java Full Stack",
+        desc: "Java Development Course with use cases and scenarios for software requirements.",
+      },
+      {
+        title: "Cloud Computing",
+        desc: "An approach to designing, constructing and operating workloads that are built in the cloud and take full advantage of Cloud computing model.",
+      },
+      {
+        title: "Python",
+        desc: "High-level, interpreted, general purpose programming with object orientation scripting.",
+      },
+      {
+        title: "C Programming",
+        desc: "General purpose programming language with developing softwares like operating systems, data bases and compilers.",
+      },
+      {
+        title: "Data Science",
+        desc: "Use case scenarios of modern tools & techniques to find unseen patterns and to make best business decisions.",
+      },
+      {
+        title: "Sales & Marketing",
+        desc: "Hands-on training on needs of the market, complete training on CRM's.",
+      },
+    ],
+  },
+  whyChooseUs: {
+    desc: "A high quality online learning experience so you can learn in-demand skills to take your career to the next level.",
+    subTopics: [
+      {
+        logo: "fas fa-chalkboard-user",
+        title: "Learn from the best",
+        desc: "We have always believed that doing our best makes us better, so we strive to provide you with a high-quality education to assist you in furthering your career.",
+      },
+      {
+        logo: "fas fa-headset",
+        title: "Personalized Support",
+        desc: "Connect with our Team to get advice on the right Course for you. Once registered, enjoy 24*7 program support.",
+      },
+      {
+        logo: "fas fa-chart-line",
+        title: "Career Acceleration",
+        desc: "We assist you in acquiring new skills by teaching you how to navigate various learning management programmes and Upskill your career.",
+      },
+      {
+        logo: "fas fa-earth-americas",
+        title: "Real-world Application",
+        desc: "You'll learn from real-life case studies and simulations so you'll be ready for future opportunities and can put what you've learned into practise right away.",
+      },
+      {
+        logo: "fas fa-laptop-file",
+        title: "Personalized Learning",
+        desc: "Our courses are available on a per-diem basis, so you can pick and choose which topics you want to learn about rather than committing to a set of lessons.",
+      },
+      {
+        logo: "fas fa-shuffle",
+        title: "Flexibility",
+        desc: "We give you the freedom to learn whenever and wherever you want, allowing you to devote more time outside of the classroom to your interests.",
+      },
+    ],
+  },
+  placedAt: {
+    title: "Our Students Placed At",
+    logos: [
+      "./img/companies/1.png",
+      "./img/companies/2.png",
+      "./img/companies/3.png",
+      "./img/companies/4.png",
+      "./img/companies/5.png",
+      "./img/companies/6.png",
+      "./img/companies/7.png",
+      "./img/companies/8.png",
+      "./img/companies/9.png",
+    ],
+  },
+  events: [
+    {
+      title:
+        "Skill Development Training-Orientation Day @ AVK College for Women,Hassan on 05/05/2022",
+      imgUri: "",
+    },
+    {
+      title: "Career Awareness Program in TACW",
+      imgUri: "",
+    },
+    {
+      title:
+        "Blood Donation Camp in Association with walk4blood on 10/04/2022 in Hassan",
+      imgUri: "",
+    },
+    {
+      title: "Gallery",
+      imgUri: "",
+    },
+  ],
+  conatctUs: {
+    title: "Write to us",
+    desc: "You will hear back from us for sure!! This could be the first step towards a brighter future.",
+    imageHome: "./img/contactUs.png",
+    number: "+91 72594 05684",
+    email: "support@theteragon.com",
+  },
+  footer: {
+    leftTitle: "Teragon",
+    rightTitle: "Follow Us",
+    middleTitle1: "Ring us",
+    middleTitle2: "Write us",
+    copyright: "Copyright @ 2023 Teragon. All rights reserved.",
+    socials: [
+      { logo: "fab fa-facebook-f", uri: "/" },
+      { logo: "fab fa-instagram", uri: "/" },
+      { logo: "fab fa-youtube", uri: "/" },
+      { logo: "fab fa-twitter", uri: "/" },
+    ],
+  },
+  error: {
+    code: 404,
+    message: "WE ARE SORRY, PAGE NOT FOUND!",
+    desc: "THE PAGE YOU ARE LOOKING FOR MIGHT HAVE BEEN REMOVED OR HAD ITS NAME CHANGED OR IS TEMPORARILY UNAVAILABLE.",
+    button: "back to homepage",
+    homeUri: "/",
+  },
+};
+
+export default config;
---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "public"),
+    filename: "main.js",
+    publicPath: "/",
+  },
+
+  target: "web",
+  devServer: {
+    port: "3000",
+    static: ["./public"],
+    open: true,
+    hot: true,
+    liveReload: true,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json", ".ts", ".css", ".ttf"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: "babel-loader",
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.js$/,
+        enforce: "pre",
+        use: ["source-map-loader"],
+      },
+    ],
+  },
+};
