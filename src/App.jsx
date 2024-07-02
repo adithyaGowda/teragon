957a32b45677fc5d1b42cd3dc2502a186c4a430c
adithyaGowda <52035861+adithyaGowda@users.noreply.github.com>
2024-07-02 09:27:33 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,39 +22,53 @@ import AccessError from "./components/AccessError";
 
 const App = () => {
   const dispatch = useDispatch();
-
-  const code = window.location.href.split("=")[1];
+  const code = new URLSearchParams(window.location.search).get("code");
+  // window.location.href.split("=")[1];
   const ClientId = "itke9a465pu2h3c03re23lvu7";
   const redirect_uri = "http://localhost:5173";
+  const [isFetchingTokens, setIsFetchingTokens] = useState(false);
 
   useEffect(() => {
-    if (code) {
-      fetch(
-        "https://ltim-de-user-pool.auth.us-east-1.amazoncognito.com/oauth2/token",
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/x-www-form-urlencoded",
-          },
-          body: `grant_type=authorization_code&code=${code}&client_id=${ClientId}&redirect_uri=${redirect_uri}`,
-        }
-      )
-        .then((response) => response.json())
-        .then((res) => {
-          const { access_token, id_token, refresh_token } = res;
+    const fetchTokens = async (code) => {
+      try {
+        setIsFetchingTokens(true);
+        const response = await fetch(
+          "https://ltim-de-user-pool.auth.us-east-1.amazoncognito.com/oauth2/token",
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/x-www-form-urlencoded",
+            },
+            body: `grant_type=authorization_code&code=${code}&client_id=${ClientId}&redirect_uri=${redirect_uri}`,
+          }
+        );
+        const res = await response.json();
+        const { access_token, id_token, refresh_token } = res;
+        if (id_token) {
           localStorage.setItem("AWS_ACCESS_TOKEN", access_token);
           localStorage.setItem("AGILE_REFRESH_TOKEN", refresh_token);
           localStorage.setItem("AGILE_ID_TOKEN", id_token);
-          // Cookies.set("AGILE_ID_TOKEN", id_token, { expires: 1, secure: true });
           const user = jwtDecode(id_token);
-          // dispatch(userProfile(user));
-          return user?.email;
-        })
-        .then((email) => dispatch(getDataFromDummy(email.toLowerCase())))
-        .catch((err) => console.log(err));
-      // .then((email) => dispatch(fetchUserRole(email)))
+          dispatch(getDataFromDummy(user.email.toLowerCase()));
+          window.history.replaceState(
+            {},
+            document.title,
+            window.location.pathname
+          );
+        } else {
+          console.error("Failed to get id_token");
+        }
+      } catch (err) {
+        console.error("Failed to fetch tokens: ", err);
+      } finally {
+        setIsFetchingTokens(false);
+      }
+    };
+
+    if (code && !localStorage.getItem("AGILE_ID_TOKEN") && !isFetchingTokens) {
+      fetchTokens(code);
     }
-  }, []);
+  }, [code, dispatch, isFetchingTokens]);
 
   const userData = useSelector((store) => store.user.userData);
 
@@ -62,32 +76,33 @@ const App = () => {
     dispatch(setIsSidebarOpen());
   };
 
-  if (!localStorage.getItem("AGILE_ID_TOKEN")) {
+  if (!localStorage.getItem("AGILE_ID_TOKEN") && !isFetchingTokens) {
     return <Login />;
   }
 
-  if (Object.keys(userData).length === 0) {
+  if (!userData || Object.keys(userData).length === 0) {
     return <AccessError />;
-  } else
-    return (
-      <div className="app-container ">
-        <Box sx={{ display: "flex", height: "100vh", flexDirection: "column" }}>
-          <Box
-            component="header"
-            sx={{
-              height: "70px",
-              borderBottom: "1px solid gray",
-            }}
-          >
-            <Header toggleSidebar={toggleSidebar} />
-          </Box>
-          <Box sx={{ display: "flex", flex: 1 }}>
-            {userData?.role === "admin" && <SideBar />}
-            <Outlet />
-            <ToastContainer />
-          </Box>
+  }
+  console.log(userData);
+  return (
+    <div className="app-container ">
+      <Box sx={{ display: "flex", height: "100vh", flexDirection: "column" }}>
+        <Box
+          component="header"
+          sx={{
+            height: "70px",
+            borderBottom: "1px solid gray",
+          }}
+        >
+          <Header toggleSidebar={toggleSidebar} />
         </Box>
-      </div>
-    );
+        <Box sx={{ display: "flex", flex: 1 }}>
+          {userData?.role === "admin" && <SideBar />}
+          <Outlet />
+          <ToastContainer />
+        </Box>
+      </Box>
+    </div>
+  );
 };
 export default App;
